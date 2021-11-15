2ab2b672aa11f029f3f1fda978deb4f68d57369c
George Ramaniuk <george.ramaniuk@gmail.com>
2021-11-15 12:50:25 +0300

    fixed lms

--- clients/lms/api.py
+++ clients/lms/api.py
@@ -19,22 +19,26 @@ class LmsClient(Client):
         self.token = token
 
     async def _handle_response(self, resp: ClientResponse) -> Tuple[dict, ClientResponse]:
-        if resp.status >= 400:
+        if resp.status != 200:
             raise LmsClientError(resp, await resp.text())
+
         try:
             data = await resp.json()
         except JSONDecodeError:
             raise LmsClientError(resp, await resp.text())
+
         return data, resp
 
     async def get_user_current(self) -> dict:
-        data, resp = await self._perform_request('get', self.get_path('v2.user.current'))
+        url = self.get_path('v2.user.current')
+        data, _ = await self._perform_request('get', url, cookies={'sessionid': self.token})
         return data
 
     async def login(self, email: str, password: str) -> str:
-        data, resp = await self._perform_request('post', self.get_path('v2.user.login'), json={'email': email, 'password': password})
-        cookies: SimpleCookie[str] = resp.cookies
+        url = self.get_path('v2.user.login')
+        _, resp = await self._perform_request('post', url, json={'email': email, 'password': password})
 
+        cookies: SimpleCookie[str] = resp.cookies
         cookie_session = cookies.get('sessionid')
         if cookie_session is None:
             raise LmsClientError(resp, await resp.text())
