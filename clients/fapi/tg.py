8eec6630bc77493027d3acc1a78a51727989e0bc
George Ramaniuk <george.ramaniuk@gmail.com>
2021-11-15 16:34:09 +0300

    fixed TgClientWithFile

--- clients/fapi/tg.py
+++ clients/fapi/tg.py
@@ -1,27 +1,35 @@
-from json import JSONDecodeError
-
 import aiohttp
+from marshmallow import ValidationError
 
 from clients.tg.api import TgClient, TgClientError
-from clients.tg.dcs import File, Message
+from clients.tg.dcs import File, Message, GetFileResponse, SendMessageResponse
 
 
 class TgClientWithFile(TgClient):
     async def get_file(self, file_id: str) -> File:
-        data = await self._perform_request('get', self.get_path('getFile'), params={"file_id": file_id})
-        file: File = File.Schema().load(data['result'])
-        return file
+        url = self.get_base_path() + '/getFile'
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params={"file_id": file_id}) as resp:
+                res = await self._handle_response(resp)
+                try:
+                    gf_response: GetFileResponse = GetFileResponse.Schema().load(res)
+                except ValidationError:
+                    raise TgClientError
+                return gf_response.result
 
     async def download_file(self, file_path: str, destination_path: str):
-        async with self.session.request('get', f'{self.get_base_path()}/file/bot{self.token}/{file_path}') as resp:
-            if resp.status >= 400:
-                raise TgClientError(resp, await resp.text())
+        url = f'{self.API_PATH}/file/bot{self.token}/{file_path}'
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                if resp.status != 200:
+                    raise TgClientError
 
-            with open(destination_path, 'wb') as fd:
-                async for data in resp.content.iter_chunked(1024):
-                    fd.write(data)
+                with open(destination_path, 'wb') as fd:
+                    async for data in resp.content.iter_chunked(1024):
+                        fd.write(data)
 
     async def send_document(self, chat_id: int, document_path) -> Message:
+        url = f'{self.get_base_path()}' + '/sendDocument'
         data = aiohttp.FormData()
         data.add_field(
             'document',
@@ -33,17 +41,16 @@ class TgClientWithFile(TgClient):
             chat_id,
             content_type='text/plain'
         )
-
-        async with self.session.request('post', self.get_path('sendDocument'), data=data) as resp:
-            if resp.status >= 400:
-                raise TgClientError(resp, await resp.text())
-
-            try:
-                data = await resp.json()
-            except JSONDecodeError:
-                raise TgClientError(resp, await resp.text())
-
-            res: Message = Message.Schema().load(data['result'])
-
-        return res
+        async with aiohttp.ClientSession() as session:
+            with open(document_path, 'rb') as fd:
+                data = aiohttp.FormData()
+                data.add_field('document', fd, content_type='text/plain')
+                data.add_field('chat_id', chat_id, content_type='text/plain')
+                async with session.post(url, data=data) as resp:
+                    res_dict = await self._handle_response(resp)
+                    try:
+                        sm_response: SendMessageResponse = SendMessageResponse.Schema().load(res_dict)
+                    except ValidationError:
+                        raise TgClientError
+                return sm_response.result
 
